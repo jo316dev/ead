96e43ba7c5c8fe8448dc18d8df477f2efb5e19a6
Ricardo <jo316.dev@gmail.com>
2022-01-21 11:04:49 -0300

    erro no sancutm

--- routes/api.php
+++ routes/api.php
@@ -18,34 +18,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth', [AuthController::class, 'auth']);
 
-Route::get('/courses', [CourseController::class, 'index']);
-
-Route::get('/courses/{id}', [CourseController::class, 'show']);
-
-Route::get('/courses/{id}/module', [ModuleController::class, 'index']);
-
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lesson/{id}', [LessonController::class, 'show']);
-
-Route::get('/supports', [SupportController::class, 'index']);
-
-Route::get('/mysupports', [SupportController::class, 'mySupports']);
-
-// Add a question
-Route::post('/supports', [SupportController::class, 'store']);
-
-// Post a reply to support
-Route::post('replies', [ReplySupportController::class, 'createReply']);
+Route::middleware(['auth:sanctum'])->group(function () {
 
 
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/courses', [CourseController::class, 'index']);
 
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
 
-Route::get('/user', function () {
+    Route::get('/courses/{id}/module', [ModuleController::class, 'index']);
 
-    $users = User::all();
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lesson/{id}', [LessonController::class, 'show']);
 
-    return response()->json($users);
+    Route::get('/supports', [SupportController::class, 'index']);
+
+    Route::get('/mysupports', [SupportController::class, 'mySupports']);
+
+    // Add a question
+    Route::post('/supports', [SupportController::class, 'store']);
+
+    // Post a reply to support
+    Route::post('replies', [ReplySupportController::class, 'createReply']);
 });
+
+
 
 Route::get('/', function () {
     return response()->json([
